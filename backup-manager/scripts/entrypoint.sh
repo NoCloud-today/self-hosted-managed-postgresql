0efa7f845425f473459e37264ec73548cede8f5b
andrewbolotsky <andrew04tlt@gmail.com>
2025-03-18 22:47:27 +0300

    delete existing stanza if exists

--- backup-manager/scripts/entrypoint.sh
+++ backup-manager/scripts/entrypoint.sh
@@ -4,7 +4,13 @@ service ssh restart
 
 su pgbackrest -c 'ssh-keyscan -H postgres >> /home/pgbackrest/.ssh/known_hosts'
 
+
 if ! su pgbackrest -c 'pgbackrest --stanza=main info >/dev/null 2>&1'; then
+    su pgbackrest -c "pgbackrest --log-level-console=info --stanza=main stanza-create"
+else
+    echo "Stanza already exist, deleting and creating new"
+    su pgbackrest -c "pgbackrest stop --stanza=main"
+    su pgbackrest -c "pgbackrest stanza-delete --stanza=main --force"
     su pgbackrest -c "pgbackrest --log-level-console=info --stanza=main stanza-create"
 fi
 
