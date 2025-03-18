c528e35a382c6467058819b075be995a6ff8c3dc
andrewbolotsky <andrew04tlt@gmail.com>
2025-03-19 00:11:24 +0300

    fix: scripts in backup-manager

--- backup-manager/scripts/restore.sh
+++ backup-manager/scripts/restore.sh
@@ -11,14 +11,12 @@ fi
 ./stop.sh
 sleep 2
 
-su pgbackrest -c 'ssh  postgres@postgres "sudo rm -rf /var/lib/postgresql/16/main/*"'
 
-if [ -z "$TARGET_TIME" ]; then
-    pgbackrest --stanza=main --log-level-console=info --delta restore
-else
-    pgbackrest --stanza=main --log-level-console=info --delta --recovery-option="recovery_target_time=${TARGET_TIME}" restore
-fi
+su pgbackrest -c "ssh -t postgres@postgres 'pgbackrest --stanza=main --log-level-console=info --type=time  \"--target=${TARGET_TIME}\" --delta restore'"
+
 
 ./start.sh
+
+
 
 
--- backup-manager/scripts/start.sh
+++ backup-manager/scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "Starting PostgreSQL..."
-su pgbackrest -c "ssh postgres@postgres \"sudo pg_ctl -D /var/lib/postgres/data/pgdata -l logfile start\"" || {
+su pgbackrest -c "ssh postgres@postgres  \"/usr/lib/postgresql/16/bin/pg_ctl -D /var/lib/postgresql/16/main -l /var/log/postgresql/postgresql.log -o '-c config_file=/etc/postgresql/16/main/postgresql.conf' start\"" || {
     echo "Failed to start PostgreSQL"
     exit 1
 }
