cff8be2b69ba9cd9f10758549b262312abe82260
andrewbolotsky <andrew04tlt@gmail.com>
2025-03-30 18:14:13 +0300

    fix: make containers save state after restart

--- backup-manager/scripts/entrypoint.sh
+++ backup-manager/scripts/entrypoint.sh
@@ -4,14 +4,12 @@ service ssh restart
 
 su pgbackrest -c 'ssh-keyscan -H postgres >> /home/pgbackrest/.ssh/known_hosts'
 
+su pgbackrest -c 'pgbackrest --stanza=main info' | cat
 
-if ! su pgbackrest -c 'pgbackrest --stanza=main info >/dev/null 2>&1'; then
+if [ ! -d "/var/lib/pgbackrest" ] || [ -z "$(ls -A "/var/lib/pgbackrest" 2>/dev/null)" ]; then
+    echo "Stanza does not exist, creating new"
     su pgbackrest -c "pgbackrest --log-level-console=info --stanza=main stanza-create"
-else
-    echo "Stanza already exist, deleting and creating new"
-    su pgbackrest -c "pgbackrest stop --stanza=main"
-    su pgbackrest -c "pgbackrest stanza-delete --stanza=main --force"
-    su pgbackrest -c "pgbackrest --log-level-console=info --stanza=main stanza-create"
+    su pgbackrest -c "pgbackrest --log-level-console=info  backup --type=full --stanza=main"
 fi
 
 service cron start
