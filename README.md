cdb7dff77cc5003cdc13b5700e7cba8b615f1902
andrewbolotsky <andrew04tlt@gmail.com>
2025-03-31 23:50:40 +0300

    feat: make first version of pgbackrest with s3

--- README.md
+++ README.md
@@ -8,5 +8,10 @@
    ssh-keygen -t rsa -b 4096 -f ssh_keys/id_rsa_postgres
    ssh-keygen -t rsa -b 4096 -f ssh_keys/id_rsa_backup_manager
    ```
-
+2. Create `certs` directory and make certificates 
+   ```bash
+   mkdir -p certs
+   openssl genpkey -algorithm RSA -out certs/private.key -pkeyopt rsa_keygen_bits:2048
+   openssl req -new -x509 -key certs/private.key -out certs/public.crt -days 3650 -subj "/CN=minio"
+   ```
 2. Run docker-compose.yml
--- backup-manager/scripts/entrypoint.sh
+++ backup-manager/scripts/entrypoint.sh
@@ -2,16 +2,9 @@
 
 service ssh restart
 
-su pgbackrest -c 'ssh-keyscan -H postgres >> /home/pgbackrest/.ssh/known_hosts'
+su backup-manager -c 'ssh-keyscan -H postgres >> /home/backup-manager/.ssh/known_hosts'
 
-su pgbackrest -c 'pgbackrest --stanza=main info' | cat
 
-if [ ! -d "/var/lib/pgbackrest" ] || [ -z "$(ls -A "/var/lib/pgbackrest" 2>/dev/null)" ]; then
-    echo "Stanza does not exist, creating new"
-    su pgbackrest -c "pgbackrest --log-level-console=info --stanza=main stanza-create"
-    su pgbackrest -c "pgbackrest --log-level-console=info  backup --type=full --stanza=main"
-fi
+su backup-manager "service cron start"
 
-service cron start
-
-exec su pgbackrest -c "uvicorn app.main:app --host 0.0.0.0 --port 8000"
+exec su backup-manager -c "uvicorn app.main:app --host 0.0.0.0 --port 8000"
