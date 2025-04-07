84f932f01db4b80ea21bf9702e7b1005d3bcb255
andrewbolotsky <andrew04tlt@gmail.com>
2025-04-07 09:30:05 +0300

    test: fix unit tests

--- test-scripts/run_unit_tests.sh
+++ test-scripts/run_unit_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running unit tests"
-docker build -f "../backup-manager.unit-test.Dockerfile" -t backup-manager-unit-test ..
+docker build -f "../backup-manager.unit-test.Dockerfile" -t backup-manager-unit-test .
 docker run --name backup-manager-unit-test-container backup-manager-unit-test
 TEST_EXIT_CODE=$?
 docker rm backup-manager-unit-test-container
