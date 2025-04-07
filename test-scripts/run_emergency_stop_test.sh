089bd72449c38505c7e86345b4f783f5758664bb
andrewbolotsky <andrew04tlt@gmail.com>
2025-04-07 11:37:22 +0300

    test: add tests

--- test-scripts/run_emergency_stop_test.sh
+++ test-scripts/run_emergency_stop_test.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-TEST_DIRECTORY="./test-volume"
-BEFORE=$DOCKER_VOLUME_DIRECTORY
-export DOCKER_VOLUME_DIRECTORY=$TEST_DIRECTORY
+
 
 wait_for_health() {
     local max_attempts=30
     local attempt=0
     echo "Waiting for health check to pass..."
-    
+
     while [ $attempt -lt $max_attempts ]; do
         attempt=$((attempt + 1))
 
@@ -16,70 +14,145 @@ wait_for_health() {
             echo "Health check passed"
             return 0
         fi
-        
+
         sleep 1
     done
     attempt=0
 }
 
-echo "Starting containers..."
-docker compose up -d --build
+prepare_test_database() {
+  echo "Starting containers..."
+  docker compose up -d --build
 
-echo "Waiting for containers to be healthy..."
-wait_for_health
+  echo "Waiting for containers to be healthy..."
+  wait_for_health
 
-echo "Creating test database..."
-curl -X POST http://0.0.0.0:8000/database/run \
-  -H "Content-Type: application/json" \
-  -d '{"query": "CREATE database test_database"}'
+  echo "Creating test database..."
+  curl -X POST http://0.0.0.0:8000/database/run \
+    -H "Content-Type: application/json" \
+    -d '{"query": "CREATE database test_database"}'
 
-echo "Creating test table..."
-curl -X POST http://0.0.0.0:8000/database/run \
-  -H "Content-Type: application/json" \
-  -d '{"query": "CREATE table test_table(id bigint)", "database_name": "test_database"}'
+  echo "Creating test table..."
+  curl -X POST http://0.0.0.0:8000/database/run \
+    -H "Content-Type: application/json" \
+    -d '{"query": "CREATE table test_table(id bigint)", "database_name": "test_database"}'
 
-echo "Inserting test data..."
-curl -X POST http://0.0.0.0:8000/database/run \
-  -H "Content-Type: application/json" \
-  -d '{"query": "INSERT INTO test_table(id) VALUES (1),(2)", "database_name": "test_database"}'
+  echo "Inserting test data..."
+  curl -X POST http://0.0.0.0:8000/database/run \
+    -H "Content-Type: application/json" \
+    -d '{"query": "INSERT INTO test_table(id) VALUES (1),(2)", "database_name": "test_database"}'
+}
 
-echo "Performing full backup..."
-curl -X POST http://0.0.0.0:8000/backup/full
+verify_restore(){
+  echo "Verifying restored data..."
+  EXPECTED_RESPONSE='{"message":"SQL executed successfully","result":"1\n2"}'
+  ACTUAL_RESPONSE=$(curl -s -X POST http://0.0.0.0:8000/database/run \
+    -H "Content-Type: application/json" \
+    -d '{"query": "SELECT * from test_table", "database_name": "test_database"}')
 
-echo "Stopping postgres container..."
-docker compose stop postgres
+  if [ "$ACTUAL_RESPONSE" != "$EXPECTED_RESPONSE" ]; then
+      echo "Test failed: Restored data verification failed"
+      echo "Expected: $EXPECTED_RESPONSE"
+      echo "Got: $ACTUAL_RESPONSE"
+      exit 1
+  fi
 
-echo "Deleting postgres data..."
-rm -rf $TEST_DIRECTORY/postgres_data/*
+  echo "Data verification successful"
+}
+wait_for_postgres_container(){
+  echo "Waiting for postgres to be ready..."
+  sleep 20
+}
+create_immediate_restore(){
+  echo "Performing immediate restore..."
+  curl -X POST http://0.0.0.0:8000/restore/immediate
+}
+create_pitr_restore(){
+  echo "Performing pitr restore..."
+  curl -X POST http://0.0.0.0:8000/restore/immediate?timestamp="$1"
+}
+cleanup(){
+  echo "Cleaning up..."
+  docker compose down -v
+  sudo rm -rf "./test-volume"
 
-echo "Starting postgres container..."
-docker compose start postgres
 
-echo "Waiting for postgres to be ready..."
-wait_for_health
-sleep 20
-echo "Performing immediate restore..."
-curl -X POST http://0.0.0.0:8000/restore/immediate
+  echo "Test completed successfully"
+}
 
-echo "Verifying restored data..."
-EXPECTED_RESPONSE='{"message":"SQL executed successfully","result":"1\n2"}'
-ACTUAL_RESPONSE=$(curl -s -X POST http://0.0.0.0:8000/database/run \
-  -H "Content-Type: application/json" \
-  -d '{"query": "SELECT * from test_table", "database_name": "test_database"}')
+full_backup_test(){
+  prepare_test_database
+  echo "Performing full backup..."
+  curl -X POST http://0.0.0.0:8000/backup/full
 
-if [ "$ACTUAL_RESPONSE" != "$EXPECTED_RESPONSE" ]; then
-    echo "Test failed: Restored data verification failed"
-    echo "Expected: $EXPECTED_RESPONSE"
-    echo "Got: $ACTUAL_RESPONSE"
-    exit 1
-fi
 
-echo "Data verification successful"
+  wait_for_postgres_container
+  create_immediate_restore
+  verify_restore
+  cleanup
+}
 
-echo "Cleaning up..."
-docker compose down -v
-sudo rm -rf $TEST_DIRECTORY
+diff_backup_test(){
+  prepare_test_database
+  echo "Performing diff backup..."
+  curl -X POST http://0.0.0.0:8000/backup/diff
 
+  wait_for_postgres_container
+  create_immediate_restore
+  verify_restore
+  cleanup
+}
+incr_backup_test(){
+  prepare_test_database
+  echo "Performing incr backup..."
+  curl -X POST http://0.0.0.0:8000/backup/incr
+
+
+  wait_for_postgres_container
+  create_immediate_restore
+  verify_restore
+  cleanup
+}
+
+pitr_incr_backup_test(){
+  prepare_test_database
+  TIME_TO_RESTORE=$(date +%s)
+  echo "Performing incr backup..."
+  curl -X POST http://0.0.0.0:8000/backup/incr
+
+  wait_for_postgres_container
+  create_pitr_restore $TIME_TO_RESTORE
+  verify_restore
+  cleanup
+}
+pitr_diff_backup_test(){
+  prepare_test_database
+  TIME_TO_RESTORE=$(date +%s)
+  echo "Performing incr backup..."
+  curl -X POST http://0.0.0.0:8000/backup/diff
+
+  wait_for_postgres_container
+  create_pitr_restore $TIME_TO_RESTORE
+  verify_restore
+  cleanup
+}
+pitr_full_backup_test(){
+  prepare_test_database
+  TIME_TO_RESTORE=$(date +%s)
+  echo "Performing incr backup..."
+  curl -X POST http://0.0.0.0:8000/backup/full
+
+  wait_for_postgres_container
+  create_pitr_restore $TIME_TO_RESTORE
+  verify_restore
+  cleanup
+}
+BEFORE=$DOCKER_VOLUME_DIRECTORY
+export DOCKER_VOLUME_DIRECTORY="./test-volume"
+full_backup_test
+diff_backup_test
+incr_backup_test
+pitr_incr_backup_test
+pitr_diff_backup_test
+pitr_full_backup_test
 DOCKER_VOLUME_DIRECTORY=$BEFORE
-
-echo "Test completed successfully" 
