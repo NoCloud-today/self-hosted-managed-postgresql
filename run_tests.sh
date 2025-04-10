7f3620dd1be2bb3f31b7e1cfc5c3a602f8d91508
andrewbolotsky <andrew04tlt@gmail.com>
2025-04-11 00:38:10 +0300

    test: delete not existing tests in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -14,9 +14,6 @@ if [  -$TEST_EXIT_CODE -ne 0 ]; then
   echo "Integration tests failed"
   exit $TEST_EXIT_CODE
 fi
-bash test-scripts/run_emergency_stop_test.sh full
-bash test-scripts/run_emergency_stop_test.sh diff
-bash test-scripts/run_emergency_stop_test.sh incr
 bash test-scripts/run_emergency_stop_test.sh pitr-full
 bash test-scripts/run_emergency_stop_test.sh pitr-diff
 bash test-scripts/run_emergency_stop_test.sh pitr-incr
