2ce0855a5f05255fb9f36fc14ca6944a99abfc3d
andrewbolotsky <andrew04tlt@gmail.com>
2025-04-10 23:36:34 +0300

    test: fix emergency stop test

--- run_tests.sh
+++ run_tests.sh
@@ -2,19 +2,25 @@
 
 set -e
 echo "Starting test environment..."
-bash run_unit_tests.sh
+bash test-scripts/run_unit_tests.sh
 TEST_EXIT_CODE=$?
 if [  -$TEST_EXIT_CODE -ne 0 ]; then
   echo "Unit tests failed"
   exit $TEST_EXIT_CODE
 fi
-bash run_integration_tests.sh
+bash test-scripts/run_integration_tests.sh
 TEST_EXIT_CODE=$?
 if [  -$TEST_EXIT_CODE -ne 0 ]; then
   echo "Integration tests failed"
   exit $TEST_EXIT_CODE
 fi
-bash run emergency_stop_test.sh
+bash test-scripts/run_emergency_stop_test.sh full
+bash test-scripts/run_emergency_stop_test.sh diff
+bash test-scripts/run_emergency_stop_test.sh incr
+bash test-scripts/run_emergency_stop_test.sh pitr-full
+bash test-scripts/run_emergency_stop_test.sh pitr-diff
+bash test-scripts/run_emergency_stop_test.sh pitr-incr
+
 TEST_EXIT_CODE=$?
 if [  -$TEST_EXIT_CODE -ne 0 ]; then
   echo "Emergency postgres stop test failed"
