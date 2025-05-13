2aec685be486aeeb2fdc9141c1c958ce64f6a8c4
andrewbolotsky <andrew04tlt@gmail.com>
2025-05-13 08:51:13 +0300

    fix: fix setting roles

--- test-scripts/run_e2e_tests.sh
+++ test-scripts/run_e2e_tests.sh
@@ -4,12 +4,10 @@ BEFORE=$DOCKER_VOLUME_DIRECTORY
 export DOCKER_VOLUME_DIRECTORY=$TEST_DIRECTORY
 
 docker compose  -f compose.s3.yml up --wait --build
-sleep 20
 docker compose  -f compose.yml up --wait --build
 
 docker logs pg
 
-sleep 20
 docker compose  -f compose.test.yml up --abort-on-container-exit --force-recreate --build
 TEST_EXIT_CODE=$?
 DOCKER_VOLUME_DIRECTORY=$BEFORE
