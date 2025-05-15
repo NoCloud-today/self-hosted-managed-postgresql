77efb9b2b7ec5b9ec3483f74624b68d813708219
andrewbolotsky <andrew04tlt@gmail.com>
2025-05-16 00:53:33 +0300

    test: change timeouts in tests

--- backup-manager-test/tests/utils/backup_client.py
+++ backup-manager-test/tests/utils/backup_client.py
@@ -8,38 +8,38 @@ class BackupClient:
         self.base_url = base_url
 
     def create_incremental_backup(self) -> dict:
-        response = requests.post(f"{self.base_url}/backup/incr", timeout=60)
+        response = requests.post(f"{self.base_url}/backup/incr", timeout=180)
         response.raise_for_status()
         print("Response:", response)
         return response.json()
 
     def create_full_backup(self) -> dict:
-        response = requests.post(f"{self.base_url}/backup/full", timeout=60)
+        response = requests.post(f"{self.base_url}/backup/full", timeout=180)
         response.raise_for_status()
         print("Response:", response)
         return response.json()
 
     def create_diff_backup(self) -> dict:
-        response = requests.post(f"{self.base_url}/backup/diff", timeout=60)
+        response = requests.post(f"{self.base_url}/backup/diff", timeout=180)
         response.raise_for_status()
         print("Response:", response)
         return response.json()
 
     def list_backups(self) -> list[dict]:
-        response = requests.get(f"{self.base_url}/backups", timeout=60)
+        response = requests.get(f"{self.base_url}/backups", timeout=180)
         response.raise_for_status()
         print("Response:", response)
         return response.json()
 
     def restore_backup_by_time(self, timestamp: int) -> dict:
-        response = requests.post(f"{self.base_url}/restore/time", params={"timestamp": timestamp}, timeout=1000)
+        response = requests.post(f"{self.base_url}/restore/time", params={"timestamp": timestamp}, timeout=180)
         response.raise_for_status()
         print("Response:", response)
         return response.json()
 
     def restore_backup_immediate(self, database_name: Optional[str] = None) -> dict:
         params = {"database_name": database_name} if database_name else {}
-        response = requests.post(f"{self.base_url}/restore/immediate", params=params, timeout=60)
+        response = requests.post(f"{self.base_url}/restore/immediate", params=params, timeout=180)
         response.raise_for_status()
         print("Response:", response)
         return response.json()
