843223c9ad8b185f8d90a3e405bb8ce0ec6cc2fa
andrewbolotsky <andrew04tlt@gmail.com>
2025-06-01 14:36:50 +0300

    fix: backup prepare script

--- backup-manager/backup_prepare.sh
+++ backup-manager/backup_prepare.sh
@@ -7,8 +7,9 @@ PG_CLUSTER="main"
 chown -R ${BACKREST_USER}:${BACKREST_GROUP} /var/run
 
 initialize_stanza(){
-  echo "Stanza does not exist, creating new"
+  echo "Stanza does not exist, creating new stanza"
   su "${BACKREST_USER}" <<'EOF'
+  PG_CLUSTER="main"
   pgbackrest --log-level-console=info --stanza=$PG_CLUSTER stanza-create
 EOF
 }
@@ -16,8 +17,9 @@ if  ! pgbackrest info | grep -q "stanza: $PG_CLUSTER"; then
     initialize_stanza
 fi
 if  pgbackrest info | grep -q "status: error"; then
-    echo "No backup exists, creating backup"
+    echo "No backup exists, creating backup in stanza"
     su "${BACKREST_USER}" <<'EOF'
+    PG_CLUSTER="main"
     pgbackrest --log-level-console=info  backup --type=full --stanza=$PG_CLUSTER --repo 1
     pgbackrest --log-level-console=info  backup --type=full --stanza=$PG_CLUSTER --repo 2
 EOF
